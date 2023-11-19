de5abbbd0d30ec15bb95ec76d593273102a59278
P1bub <109060788+AnonymoDGH@users.noreply.github.com>
2023-11-19 03:10:23 -0500

    Update main.js

--- src/mineflayer/main.js
+++ src/mineflayer/main.js
@@ -1,1 +1,19 @@
-require('mineflayer');
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+  host: 'localhost',
+  username: 'Bot', 
+  auth: 'microsoft' 
+  port: 25565,                
+  version: false,             
+  password: '12345678'        
+})
+
+bot.on('chat', (username, message) => {
+  if (username === bot.username) return
+  bot.chat(message)
+})
+
+// Log errors and kick reasons:
+bot.on('kicked', console.log)
+bot.on('error', console.log)
